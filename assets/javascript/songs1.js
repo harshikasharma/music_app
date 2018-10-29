29c2498ec9a4e3132adab0b8499181153209119c
harshikasharma <40965390+harshikasharma@users.noreply.github.com>
2018-10-30 00:19:12 +0530

    Add files via upload

---
+++ assets/javascript/songs1.js
@@ -0,0 +1,77 @@
+var songsArray1 = [
+    {
+        'songId' : 1,
+        'songName' : 'Aaya Na Tu',
+        'songUrl' :  'assets/songs/Aaya Na Tu Arjun Kanungo-(Mp3Sun.Com).mp3',
+        'songImage' : 'https://c.saavncdn.com/806/Aaya-Na-Tu-Hindi-2018-20180618230244-150x150.jpg'
+    },
+    {
+        'songId' : 2,
+        'songName' : 'High Rated',
+        'songUrl' :  'assets/songs/01 High Rated Gabru - Nawabzaade (SongsMp3.Com).mp3',
+        'songImage' : 'https://c.saavncdn.com/326/Nawabzaade-Hindi-2018-20180717-150x150.jpg'
+    },
+    {
+        'songId' : 3,
+        'songName' : 'Dilbar',
+        'songUrl' :  'assets/songs/01 Dilbar - Satyameva Jayate (SongsMp3.Com).mp3',
+        'songImage' : 'https://c.saavncdn.com/333/Satyameva-Jayate-Hindi-2018-20180801-150x150.jpg'
+    },
+    {
+        'songId' : 4,
+        'songName' : 'One Day',
+        'songUrl' :  'assets/songs/Tareefan.mp3',
+        'songImage' : 'https://c.saavncdn.com/197/One-Day-English-2018-20180725233551-150x150.jpg'
+
+    },
+    {
+        'songId' : 5,
+        'songName' : 'Dhadak',
+        'songUrl' :  'assets/songs/01 Dhadak - Title Song (SongsMp3.Com).mp3',
+        'songImage' : 'https://c.saavncdn.com/070/Dhadak-Hindi-2018-20180814-150x150.jpg'
+    },
+    {
+        'songId' : 6,
+        'songName' : 'Tu Yahan',
+        'songUrl' :  'assets/songs/Tareefan.mp3',
+        'songImage' : 'https://c.saavncdn.com/257/Tu-Yahaan-Single-Hindi-2018-20180710132709-150x150.jpg'
+    },
+    {
+        'songId' : 7,
+        'songName' : 'Kalesh',
+        'songUrl' :  'assets/songs/Faded.mp3',
+        'songImage' : 'https://c.saavncdn.com/389/Kalesh-Hindi-2018-20180816-150x150.jpg'
+    },
+    {
+        'songId' : 8,
+        'songName' : 'Party Over',
+        'songUrl' :  'assets/songs/This-Party-Is-Over-Now-Yo-Yo-Honey-Singh.mp3',
+        'songImage' : 'https://c.saavncdn.com/660/This-Party-Is-Over-Now-From-Mitron--Hindi-2018-20180813114036-150x150.jpg'
+    },
+    {
+        'songId' : 9,
+        'songName' : 'Manmarizayan',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/821/Manmarziyaan-Hindi-2018-20180817-150x150.jpg'
+    },
+ {
+    
+        'songId' : 10,
+        'songName' : 'Roots',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/738/Roots-Hindi-2018-20180726115927-150x150.jpg'
+    },
+    {
+        'songId' : 11,
+        'songName' : 'Pretty Girl',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/819/Pretty-Girl-Hindi-2018-20180719-150x150.jpg'
+    },
+
+    {
+         'songId' : 12,
+         'songName' : 'Seedha Saadha',
+         'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+         'songImage' : 'https://c.saavncdn.com/266/Seedha-Saadha-Hindi-2018-20180709172239-150x150.jpg'
+     },
+    ]
---
+++ assets/javascript/songs2.js
@@ -0,0 +1,76 @@
+var songsArray2 = [
+    {
+        'songId' : 1,
+        'songName' : 'Arijit Love',
+        'songUrl' :  'assets/songs/27 - Arijit Singh (GiMA Live Mashup) (SongsMp3.Com).mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/AllArijit_20180426001900_150x150.jpg'
+    },
+    {
+        'songId' : 2,
+        'songName' : 'Neha Kakkar',
+        'songUrl' :  'assets/songs/01 Dilbar - Satyameva Jayate (SongsMp3.Com).mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/464932_hindi_20180806192737_150x150.jpg'
+    },
+    {
+        'songId' : 3,
+        'songName' : 'Best of 2017',
+        'songUrl' :  'assets/songs/Shape Of Bollywood - Best Of 2017 Mashup.mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/TopBollywoodSongs2017byOnePlus_99978301_20180102_150x150.jpg'
+    },
+    {
+        'songId' : 4,
+        'songName' : 'Sunidhi Chauhan',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/TheBestofSunidhiChauhan_20180803045201_150x150.jpg'
+    },
+    {
+        'songId' : 5,
+        'songName' : 'Remix',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/TheRemix_3172464_20161222_150x150.jpg'
+    },
+    {
+        'songId' : 6,
+        'songName' : 'Icons - Kishore',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/IconsKishoreKumar_20180801095752_150x150.jpg'
+    },
+    {
+        'songId' : 7,
+        'songName' : 'Fresh Hits',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/FreshHits_20180815123654_150x150.jpg'
+    },
+    {
+        'songId' : 8,
+        'songName' : 'Rim Jhim',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/RimJhimRimJhim_99978094_20170530_150x150.jpg'
+    },
+    {
+        'songId' : 9,
+        'songName' : 'Love in 2017',
+        'songUrl' :  'assets/songs/27 - Arijit Singh (GiMA Live Mashup) (SongsMp3.Com).mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/BestLoveSongs2017byOnePlus_99978299_20180103_150x150.jpg'
+    },
+ {
+    
+        'songId' : 10,
+        'songName' : 'KK',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/KKsBest_20180817172402_150x150.jpg'
+    },
+    {
+        'songId' : 11,
+        'songName' : 'Sonu Nigam All',
+        'songUrl' :  'assets/songs/titanic my heart will go instrumental.mp3',
+        'songImage' : 'https://c.saavncdn.com/editorial/SonuNigamAllTimeHits_20180731010821_150x150.jpg'
+    },
+
+    {
+         'songId' : 12,
+         'songName' : 'Atif Awaz',
+         'songUrl' :  'assets/songs/Kya Se Kya Ho Gaye Dekhte Dekhte_64(DjPaji.Com).mp3',
+         'songImage' : 'https://c.saavncdn.com/editorial/AtifKiAwaaz_99972547_201803131520932569227_150x150.jpg'
+     },
+    ]
